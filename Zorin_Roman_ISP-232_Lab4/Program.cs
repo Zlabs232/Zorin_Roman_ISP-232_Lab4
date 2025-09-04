31c73e461298152e93cbd8de38cb7c61ae8590d2
Роман Зорин <sgena170315@gmail.com>
2025-09-04 18:41:45 +0300

    Add project files.

---
+++ Zorin_Roman_ISP-232_Lab4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab4
+{
+    class Lab4
+    {
+        static void Main()
+        {
+            int[] empty_m; //Пустой массив
+            int[] empty_m2 = new int[4];  //Присвоение значения массиву 
+
+            int[] nums2 = new int[4] { 1, 2, 3, 5 };
+            int[] nums3 = new int[] { 1, 2, 3, 5 };
+            int[] nums4 = new[] { 1, 2, 3, 5 };
+            int[] nums5 = { 1, 2, 3, 5 };
+            int[] nums6 = [1, 2, 3, 5]; 
+        }
+    }
+}
