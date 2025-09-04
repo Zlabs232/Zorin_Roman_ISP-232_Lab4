5bc764d5c32c176bd3f64790bec3e1d5d73ea1b6
Роман Зорин <sgena170315@gmail.com>
2025-09-04 18:46:13 +0300

    Добавлен пример индексации

--- Zorin_Roman_ISP-232_Lab4/Program.cs
+++ Zorin_Roman_ISP-232_Lab4/Program.cs
@@ -11,7 +11,13 @@
             int[] nums3 = new int[] { 1, 2, 3, 5 };
             int[] nums4 = new[] { 1, 2, 3, 5 };
             int[] nums5 = { 1, 2, 3, 5 };
-            int[] nums6 = [1, 2, 3, 5]; 
+            int[] nums6 = [1, 2, 3, 5];
+
+            Console.WriteLine(nums5[3]);
+            var n = nums6[0];
+            Console.WriteLine(n); //Пример индексация
+
+
         }
     }
 }
